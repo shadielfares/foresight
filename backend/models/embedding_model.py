15ab8c957728e0cdbccc6d206d07d4be75c4809c
shadielfares <shadi.elfares@gmail.com>
2025-07-13 23:20:58 -0400

    Configured QA model, Text-Splitter, and Embedding Model
    i mean it works.. but its dogshit and it keeps suggesting Tim Ferris as a role

--- backend/models/embedding_model.py
+++ backend/models/embedding_model.py
@@ -1,8 +1,18 @@
 from sentence_transformers import SentenceTransformer
 
+"""
+Will have to have:
+    1. Text-splitter that can handle large texts
+    2. Embedding model that can take in text and return embeddings
+    3. Similarity function to compare embeddings (for overlap % 10-20% overlap)
+
+TODO: On a different note, I'll need something to generate a role_hint, there has to to be something to label the embeddings with a role hint 
+"""
+
 # 1. Load a pretrained Sentence Transformer model
 model = SentenceTransformer("all-MiniLM-L6-v2")
-
+#Currently 256, for optimal performance, we should keep the input text length below this value.
+max_seq_length = model.max_seq_length 
 # The sentences to encode
 sentences = [
     "The weather is lovely today.",
@@ -10,6 +20,8 @@ sentences = [
     "He drove to the stadium.",
 ]
 
+
+"""
 # 2. Calculate embeddings by calling model.encode()
 embeddings = model.encode(sentences)
 print(embeddings.shape)
@@ -18,4 +30,4 @@ print(embeddings.shape)
 # 3. Calculate the embedding similarities
 similarities = model.similarity(embeddings, embeddings)
 print(similarities)
-
+"""
